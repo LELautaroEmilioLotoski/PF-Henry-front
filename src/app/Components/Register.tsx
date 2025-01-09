82e4a568607c822062b9f55016142c3f170998f8
PedroSzyk <szykp13@gmail.com>
2025-01-08 21:26:46 -0300

    modificacion register

--- src/app/Components/Register.tsx
+++ src/app/Components/Register.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Register = () => {
   return (
     <div>Register</div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A tenetur officia aperiam praesentium repudiandae at iste atque sed soluta natus voluptatum id, illo debitis quam. Eligendi neque assumenda facere ea.</p>
   )
 }
 
