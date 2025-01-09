a7bc52acd77463e3a41b7c21cfa52d28532c4296
PedroSzyk <szykp13@gmail.com>
2025-01-08 22:40:46 -0300

    agrege un segundo p a register

--- src/app/Components/Register.tsx
+++ src/app/Components/Register.tsx
@@ -4,6 +4,7 @@ const Register = () => {
   return (
     <div>Register</div>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A tenetur officia aperiam praesentium repudiandae at iste atque sed soluta natus voluptatum id, illo debitis quam. Eligendi neque assumenda facere ea.</p>
+    <p>segunda prueba</p>
   )
 }
 
