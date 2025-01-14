d68ecc084d14028ee2ee5bdb7437be8db95e3a70
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-01-14 00:33:04 -0300

    implementando cambios

--- src/components/specific/Dashboard/UserDashboard.tsx
+++ src/components/specific/Dashboard/UserDashboard.tsx
@@ -1,22 +1,22 @@
 "use client"
 import React from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
-
+ 
 const UserDashboard = () => {
   const { user, isLoading, error } = useUser();
-
+ 
   const handleLogout = () => {
     window.location.href = "/api/auth/logout";
   };
-
+ 
   return (
     <div>
       {user && (
         <div>
-          <div className="p-20">
+          <div className="flex bg-red-400">
             <h1 className="text-2xl">Datos del usuario</h1>
-            <div className="p-20">
-              <h2>Username: {user.name}</h2>
+            <div className="bg-red-400">
+              <h2 className="text-2xl">Username: {user.name}</h2>
               <p>Email: {user.email}</p>
               <h3>NickName: {user.nickname}</h3>
               <button onClick={handleLogout}>Cerrar sesion</button>
@@ -27,5 +27,5 @@ const UserDashboard = () => {
     </div>
   );
 };
-
+ 
 export default UserDashboard;
