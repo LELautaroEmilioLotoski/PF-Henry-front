094463a606111cd509c251dee62c45d06417965d
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-01-18 18:00:50 -0300

    cambios pull request

--- src/app/Contexts.tsx
+++ src/app/Contexts.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+import { CartContextProvider } from "@/context/CartContext";
 import { UserContextProvider } from "@/context/UserContext";
-=======
-import { CartContextProvider } from "@/contexts/CartContext";
-import { UserContextProvider } from "@/contexts/UserContext";
->>>>>>> bab681db52d4f9690989c4ea0541d702fd0b452e
 import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const Contexts = ({ children }: { children: React.ReactNode }) => {
