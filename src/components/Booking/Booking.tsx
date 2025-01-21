09761c59df1fc608fc568f2b60e113c1a7a97d96
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-01-21 18:50:39 -0300

    Implementando cambio de next

--- src/components/Booking/Booking.tsx
+++ src/components/Booking/Booking.tsx
@@ -1,60 +1,65 @@
-'use client'
-import { useState } from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { CalendarIcon } from 'lucide-react'
-import { Calendar } from "@/components/ui/calendar"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { format } from "date-fns"
-import { reservation } from '@/helpers/auth.helper'
-import { useUserContext } from '@/context/UserContext'
- 
+"use client";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { format } from "date-fns";
+import { reservation } from "@/helpers/auth.helper";
+import { useUserContext } from "@/context/UserContext";
+
 export default function CreateReservation() {
-  const {userNormal} = useUserContext()
-  const [date, setDate] = useState<Date>()
-  const [time, setTime] = useState("")
-  const [guests, setGuests] = useState(1)
- 
-  const userId = userNormal?.id
- 
-  if(!userId) return null;
- 
-  const handleSubmit = async(e: React.FormEvent) => {
-    if(!date || !time || guests < 1){
-      alert("completa todos los datos por favor")
+  const { userNormal } = useUserContext();
+  const [date, setDate] = useState<Date>();
+  const [time, setTime] = useState("");
+  const [guests, setGuests] = useState(1);
+
+  const userId = userNormal?.id;
+
+  if (!userId) return null;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    if (!date || !time || guests < 1) {
+      alert("completa todos los datos por favor");
       return;
     }
- 
+
     const userData = {
       date: date.toISOString().split("T")[0],
       time,
       guest: guests,
     };
- 
-    e.preventDefault()
+
+    e.preventDefault();
     try {
       const res = await reservation(userId, userData);
-      alert("reserva creada correctamente")
+      alert("reserva creada correctamente");
       setDate(undefined);
       setTime("");
-      setGuests(1)
+      setGuests(1);
     } catch (error) {
       console.log("error" + error);
- 
     }
- 
-  }
- 
+  };
+
   return (
     <form onSubmit={handleSubmit} className="m-auto space-y-4 max-w-4xl border">
       <div>
-        <Label>Fecha</Label>
+        <Label>Date</Label>
         <Popover>
           <PopoverTrigger asChild>
-            <Button variant="outline" className="w-full justify-start text-left font-normal">
+            <Button
+              variant="outline"
+              className="w-full justify-start text-left font-normal"
+            >
               <CalendarIcon className="mr-2 h-4 w-4" />
-              {date ? format(date, "PPP") : <span>Selecciona una fecha</span>}
+              {date ? format(date, "PPP") : <span>Select a date</span>}
             </Button>
           </PopoverTrigger>
           <PopoverContent className="w-auto p-0">
@@ -68,25 +73,25 @@ export default function CreateReservation() {
         </Popover>
       </div>
       <div>
-        <Label htmlFor="time">Hora</Label>
-        <Input 
-          type="time" 
-          id="time" 
+        <Label htmlFor="time">Hour</Label>
+        <Input
+          type="time"
+          id="time"
           value={time}
           onChange={(e) => setTime(e.target.value)}
         />
       </div>
       <div>
-        <Label htmlFor="guests">Número de Invitados</Label>
-        <Input 
-          type="number" 
-          id="guests" 
-          min={1} 
+        <Label htmlFor="guests">Number of Guests</Label>
+        <Input
+          type="number"
+          id="guests"
+          min={1}
           value={guests}
           onChange={(e) => setGuests(parseInt(e.target.value))}
         />
       </div>
-      <Button type="submit">Crear Reserva</Button>
+      <Button type="submit">Create reservation</Button>
     </form>
-  )
+  );
 }
