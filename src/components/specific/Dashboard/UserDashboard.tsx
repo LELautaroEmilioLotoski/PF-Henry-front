03d0587d92813a057b9851883a43a90f7a1112b7
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-01-20 23:18:04 -0300

    Corrigiendo errores de auth0

--- src/components/specific/Dashboard/UserDashboard.tsx
+++ src/components/specific/Dashboard/UserDashboard.tsx
@@ -4,33 +4,52 @@ import React from "react";
 import { useUserContext } from "@/context/UserContext";
 import DashboardSidebar from "@/components/header/Header";
 import { IUser } from "@/interfaces/Types";
+import { useUser } from "@auth0/nextjs-auth0/client";
 
 const UserDashboard = () => {
   const { userNormal, logoutUser } = useUserContext();
+  const { user, error, isLoading } = useUser();
 
   const handleLogout = () => {
     logoutUser();
     window.location.href = "/api/auth/logout";
   };
 
-  const UserData = ({ user }: { user: IUser }) => (
-    <div className="bg-gray-50 rounded-lg p-6 shadow">
-      <h2 className="text-xl font-semibold text-gray-800 mb-4">Tus datos personales</h2>
-      <p className="text-lg font-medium text-gray-600">
-        Name: <span className="text-gray-800">{user.name}</span>
-      </p>
-      <p className="text-lg font-medium text-gray-600">
-        Email: <span className="text-gray-800">{user.email}</span>
-      </p>
-      <p className="text-lg font-medium text-gray-600">
-      Address: <span className="text-gray-800">{user.address}</span>
-      </p>
-    </div>
-  );
+  // const UserData = ({ user }: { user: IUser }) => (
+  //   <div className="bg-gray-50 rounded-lg p-6 shadow">
+  //     <h2 className="text-xl font-semibold text-gray-800 mb-4">Tus datos personales</h2>
+  //     <p className="text-lg font-medium text-gray-600">
+  //       Name: <span className="text-gray-800">{user.name}</span>
+  //     </p>
+  //     <p className="text-lg font-medium text-gray-600">
+  //       Email: <span className="text-gray-800">{user.email}</span>
+  //     </p>
+  //     <p className="text-lg font-medium text-gray-600">
+  //     Address: <span className="text-gray-800">{user.address}</span>
+  //     </p>
+  //   </div>
+  // );
 
   return (
     <div className="flex justify-center bg-gray-100 py-10">
       <div className="min-h-screen bg-gray-100 flex flex-col items-center">
+        {
+          user ? (
+            <div className="bg-gray-50 rounded-lg p-6 shadow">
+            <h2 className="text-xl font-semibold text-gray-800 mb-4">Tus datos personales</h2>
+            <p className="text-lg font-medium text-gray-600">
+              Name: <span className="text-gray-800">{user.name}</span>
+            </p>
+            <p className="text-lg font-medium text-gray-600">
+              Email: <span className="text-gray-800">{user.email}</span>
+            </p>
+          </div>
+          ) : (
+            <div>
+              <h2>No se encontró ningun resultado</h2>
+            </div>
+          ) 
+        }
         {userNormal ? (
           <div className="flex">
             <DashboardSidebar />
@@ -38,7 +57,6 @@ const UserDashboard = () => {
               <h1 className="text-3xl font-semibold text-gray-800 mb-6">
                 Bienvenido, {userNormal.name}
               </h1>
-              <UserData user={userNormal} />
               <button
                 onClick={handleLogout}
                 className="bg-red-500 text-white py-2 px-4 rounded-lg hover:bg-red-600 transition duration-300"
