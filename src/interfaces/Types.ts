75a458c3b1ac023260b05716ec82e6104e47b8d7
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-02-05 01:24:20 -0300

    implementando cambios para el deploy parte 8

--- src/interfaces/Types.ts
+++ src/interfaces/Types.ts
@@ -117,7 +117,7 @@ export interface UserProps {
   email: string;
   image_url?: string;
 }
-export default interface FileUploadProps {
+export interface FileUploadProps {
   userprops: {
     email: string;
     image_url?: string;
