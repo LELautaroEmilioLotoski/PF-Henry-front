80c8744877ca5829fac052fafc4f7a82b2d7fcd4
LELautaroEmilioLotoski <lautilotoski@gmail.com>
2025-02-05 21:18:38 -0300

    implementando cambios de vercel

--- src/app/Cloudinary/page.tsx
+++ src/app/Cloudinary/page.tsx
@@ -131,24 +131,24 @@ export interface FileUploadProps {
   };
 }
 
-const FileUploadComponent = (FileUploadProps: FileUploadProps) => {
+const FileUploadComponent = ({userprops}: FileUploadProps) => {
   const [file, setFile] = useState<File | null>(null);
   const [uploading, setUploading] = useState(false);
   const [fileUrl, setFileUrl] = useState(
-    FileUploadProps?.userprops?.image_url ||
+    userprops?.image_url ||
       "https://e7.pngegg.com/pngimages/178/595/png-clipart-user-profile-computer-icons-login-user-avatars-monochrome-black-thumbnail.png"
   );
   const [showModal, setShowModal] = useState(false);
   const fileInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
-    if (FileUploadProps?.userprops?.email) {
-      const storedImageUrl = localStorage.getItem(`profileImageUrl_${FileUploadProps?.userprops?.email}`);
+    if (userprops?.email) {
+      const storedImageUrl = localStorage.getItem(`profileImageUrl_${userprops?.email}`);
       if (storedImageUrl) {
         setFileUrl(storedImageUrl);
       }
     }
-  }, [FileUploadProps?.userprops?.email]);
+  }, [userprops?.email]);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files?.[0] || null;
@@ -174,7 +174,7 @@ const FileUploadComponent = (FileUploadProps: FileUploadProps) => {
       formData.append("file", file);
 
       const response = await axios.post(
-        `http://localhost:3000/users/${FileUploadProps?.userprops?.email}/upload`,
+        `http://localhost:3000/users/${userprops?.email}/upload`,
         formData,
         {
           headers: { "Content-Type": "multipart/form-data" },
@@ -182,7 +182,7 @@ const FileUploadComponent = (FileUploadProps: FileUploadProps) => {
       );
 
       alert("Archivo subido correctamente.");
-      localStorage.setItem(`profileImageUrl_${FileUploadProps?.userprops?.email}`, response.data.img);
+      localStorage.setItem(`profileImageUrl_${userprops?.email}`, response.data.img);
       setFileUrl(response.data.img);
     } catch (error) {
       console.error("Error al subir el archivo:", error);
